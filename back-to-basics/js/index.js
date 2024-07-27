515da76446ebef71e9cba6e439d056f5145735c9
JavadiSM <1remehr@gmail.com>
2024-07-27 19:17:19 +0330

    variables in js

--- back-to-basics/js/index.js
+++ back-to-basics/js/index.js
@@ -1,6 +1,13 @@
-console.log(`hello`);
-console.log(`second`);
-document.getElementById("first-header").textContent = `hello`;
-document.getElementById("first-paragraph").textContent = `lorem`
-window.alert(`alert!`);
-window.alert(`2 alert!`);
+let x;
+x = 100;
+console.log(x);
+let age = 19;
+console.log(`I am ${age} years old ${typeof age}`);
+let firstName = "first name";
+console.log(`huh ${firstName}`);
+let online = true;
+console.log(`status: ${online}`);
+document.getElementById("p1").textContent = `your name is ${firstName};`
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = `online: ${online}`;
+
