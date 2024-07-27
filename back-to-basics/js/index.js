6e618d51d867f5adf1fcc8afd1a411e0fcd2a6b7
JavadiSM <1remehr@gmail.com>
2024-07-27 19:04:03 +0330

    js intro, logs and elements

---
+++ back-to-basics/js/index.js
@@ -0,0 +1,6 @@
+console.log(`hello`);
+console.log(`second`);
+document.getElementById("first-header").textContent = `hello`;
+document.getElementById("first-paragraph").textContent = `lorem`
+window.alert(`alert!`);
+window.alert(`2 alert!`);
