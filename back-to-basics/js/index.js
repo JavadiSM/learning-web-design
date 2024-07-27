7a40e7035e42bef61d8a48c2014608cf9fde1620
JavadiSM <1remehr@gmail.com>
2024-07-27 21:25:59 +0330

    Math and random in js

--- back-to-basics/js/index.js
+++ back-to-basics/js/index.js
@@ -1,9 +1,3 @@
-//easy way to accept input:
-// let firstName = window.prompt("your name?");
-// window.alert(`welcome ${firstName}`);
-// but THIS IS THE WAY(best):
-let username;
-document.getElementById("usernameSubmit").onclick = function () {
-    username = document.getElementById("username").value;
-    document.getElementById("myh1").textContent = "hello dear" + username;
+function generateRandomInt(min, max) {
+    return Math.floor(Math.random * (max - min)) + min;
 }
