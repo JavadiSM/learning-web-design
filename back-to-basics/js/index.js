c35e323956b110a077b609d95d2694cd451b4aeb
JavadiSM <1remehr@gmail.com>
2024-08-05 21:09:11 +0330

    radio boxes and check boxes in js

--- back-to-basics/js/index.js
+++ back-to-basics/js/index.js
@@ -1,3 +1,29 @@
 function generateRandomInt(min, max) {
     return Math.floor(Math.random * (max - min)) + min;
 }
+
+function submitInfo() {
+    if (checkbox.checked) {
+        if (studentRadio.checked){
+            result.textContent = `checked as student!`
+        } else if(parentsRadio.checked){
+            result.textContent = `checked as parent!`
+        } else {
+            result.textContent = `checked as teacher!`
+        }
+    } else {
+        result.textContent = `not checked`
+    }
+}
+
+const result = document.getElementById("result");
+const checkbox = document.getElementById("isChecked");
+
+const studentRadio = document.getElementById("student");
+const parentsRadio = document.getElementById("parents");
+const teacherRadio = document.getElementById("teacher");
+
+const submitBtn = document.getElementById("submit");
+submitBtn.onclick = function (){
+    submitInfo()
+}
