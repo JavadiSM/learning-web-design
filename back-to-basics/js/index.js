34a2a6e1f21e9edf99484e56841e1dcf6314692a
JavadiSM <1remehr@gmail.com>
2024-08-06 20:13:43 +0330

    rest parameters js

--- back-to-basics/js/index.js
+++ back-to-basics/js/index.js
@@ -2,15 +2,36 @@ function generateRandomInt(min, max) {
     return Math.floor(Math.random * (max - min + 1)) + min;
 }
 
-const array_number = [1, 2, 3];
-let max = Math.max(...array_number);
-console.log(max, typeof max);
+function printFoods(...foods) {
+    console.log(foods);
+}
 
-const username = "cool";
-let letters = [...username].join('_');
-console.log(letters);
+function createArray(...array) {
+    return array
+}
 
-let fruits = ["apple","orange","banana"];
-let vegetables = ["carrots","cabbage"];
-let all = [...fruits,...vegetables,"milk"];
-console.log(all)
+function sum(...array) {
+    let sum = 0;
+    for (let element of array) {
+        sum += element;
+    }
+    return sum;
+}
+
+function average(...array) {
+    return sum(...array) / array.length;
+}
+
+function combineStrings(...array) {
+    return array.join("<->")
+}
+
+const food1 = "a";
+const food2 = "b";
+const food3 = "c";
+const food4 = "d";
+printFoods(food1, food2, food3, food4);
+console.log(createArray(food1, food2, food3, food4));
+console.log(sum(1, 3, 53, 2));
+console.log(average(1, 3, 53, 2));
+console.log(combineStrings("aa","bb","aa"));
