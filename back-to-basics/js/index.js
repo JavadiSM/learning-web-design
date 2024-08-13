2da0c88ab3c412032a07eda8816239e99130a521
JavadiSM <1remehr@gmail.com>
2024-08-13 20:57:00 +0330

    classes js

--- back-to-basics/js/index.js
+++ back-to-basics/js/index.js
@@ -1,20 +1,22 @@
-function Person(name, age, job, isEmployed) {
-    this.name = name,
-        this.age = age,
-        this.job = job,
-        this.isEmployed = isEmployed,
-        this.hire = function () {
-            this.isEmployed = true;
-        }
+class Person {
 
+    constructor(name, age, job, isEmployed) {
+        this.name = name,
+            this.age = age,
+            this.job = job,
+            this.isEmployed = isEmployed
+    }
+
+    show() {
+        console.log(`name = ${this.name}\nage = ${this.age}\njob = ${this.job}`)
+    }
 }
 
 let david = new Person("David", 23, "unemployed", false);
 let ali = new Person("Ali", 19, "driver", true);
 console.log(david);
 console.log(ali);
-david.hire()
-console.log(`david is now employed:${david.isEmployed}`);
+david.show();
 
 function generateRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
